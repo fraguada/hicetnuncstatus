cf83d130f9e2aeee3f2b7fa222bd5160f0d35f82
Luis Fraguada <fraguada@gmail.com>
2021-03-16 11:22:47 +0100

    updated feed fetch headers

--- routes/index.js
+++ routes/index.js
@@ -8,32 +8,22 @@ async function siteStatus() {
 }
 
 async function feedStatus() {
+                           
   const res = await fetch('https://51rknuvw76.execute-api.us-east-1.amazonaws.com/dev/feed', {
-    'headers': {
-      'accept': 'application/json, text/plain, */*',
-      'accept-language': 'en-US,en;q=0.9',
-      'content-type': 'application/json;charset=UTF-8',
-      'sec-ch-ua': '\'Google Chrome\';v=\'89\', \'Chromium\';v=\'89\', \';Not A Brand\';v=\'99\'',
-      'sec-ch-ua-mobile': '?0',
-      'sec-fetch-dest': 'empty',
-      'sec-fetch-mode': 'cors',
-      'sec-fetch-site': 'cross-site'
-    },
-    'referrer': 'https://www.hicetnunc.xyz/',
-    'referrerPolicy': 'strict-origin-when-cross-origin',
     'body': '{\'counter\':0}',
-    'method': 'POST',
-    'mode': 'cors'
+    'method': 'POST'
   })
+
   return res
 
 }
 
-/* GET home page. */
 router.get('/', async function (req, res, next) {
 
-  let site = await siteStatus()
-  let feed = await feedStatus()
+  const site = await siteStatus()
+  const feed = await feedStatus()
+
+  console.log(feed)
 
   const emojiSite = ( site.ok ) ? '👍':'👎'
   const emojiFeed = ( feed.ok ) ? '👍':'👎'
